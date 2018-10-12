90239f12e0ad6bf9761327fa395e631b7d6ba75a
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-10-12 11:09:49 +0800

    装饰模式

---
+++ src/main/java/com/bonaparte/interfacet/ChargeMoneyInterface.java
@@ -0,0 +1,9 @@
+package com.bonaparte.interfacet;
+
+/**
+ * Created by yangmingquan on 2018/10/12.
+ */
+public interface ChargeMoneyInterface {
+    //收钱
+    public void chargeMoney(String data);
+}
---
+++ src/main/java/com/bonaparte/interfacet/WeixinChargeMoneyService.java
@@ -0,0 +1,11 @@
+package com.bonaparte.interfacet;
+
+/**
+ * Created by yangmingquan on 2018/10/12.
+ * 微信支付
+ */
+public class WeixinChargeMoneyService implements ChargeMoneyInterface{
+    public void chargeMoney(String data) {
+        //微信收费
+    }
+}
